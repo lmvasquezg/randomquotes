b260673b041bedc86fd7e8a37d54f33bffe62046
lmvasquezg <lmvasquezg@eafit.edu.co>
2020-05-11 11:55:31 -0500

    Random quotes con imagenes

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Random quotes</title>
+    <link rel="shortcut icon" href="https://randomquotes.s3.amazonaws.com/icon.png">
+</head>
+<body>
+    <img src="{{ $data['src'] }}" width = 30%>
+    <label> Host: {{ $data['host'] }} </label>
+</body>
+
+</html>
